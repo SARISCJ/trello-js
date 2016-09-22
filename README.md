51f2b74375b8a50574f88641a683886b865af410
SARISCJ <saracastillo0@gmail.com>
2016-09-22 17:02:31 -0500

    segunda version

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 Esta es mi tarea de TRELLO! :+1:
 
 ### VERSIÓN 0.0.1 
-Tarea: Un elemento en el HTML con el mensaje "Añadir una lista", que al dar click muestre un input y un botón (formulario) para que el usuario ingrese el nombre de la lista.
+Tarea: 
+	* Un elemento en el HTML con el mensaje "Añadir una lista", que al dar click muestre un input y un botón (formulario) para que el usuario ingrese el nombre de la lista.
 
 ## Solución:
 Para eso creamos un evento que primero cargue la pagina y luego la funcion con el evento click, y a la vez ocultar, 
 ya que cuando el usuario ingrese el nombre y haga click, aparece mi formulario y desaparece mi boton añadir una lista..!
 
+### VERSIÓN 0.0.2
+Tarea: 
+	* Mostrar en el HTML, el texto ingresado al dar click en el botón de "Guardar" del formulario (como si fuera título de la lista).
+	* Debajo del título, mostrar el mensaje clickeable de "Añadir una tarjeta".
+
+## Solución:
+Para eso cuando el usuario da clic en guardad, ocultamos la entrada y el boton, luego creamos un nuevo elemento div, insertamos el texto en el HTML como titulo de nuestro nuevo board.
+Luego creamos un nuevo elemento para que el usuario siga creando nuevas tarjetas. 
--- js/app.js
+++ js/app.js
@@ -1,25 +1,49 @@
 window.addEventListener("load", function() {
 
-    var taskContainer = document.getElementById("task-container");
-    var addTask = document.getElementById("boton-task");
+    var container = document.getElementById("task-container");
+    var boton = document.getElementById("boton-task");
+    var board = document.getElementById("board");
     
-    addTask.addEventListener("click", function() {
-    addTask.classList.add("ocultar");
-      addForm();
+    boton.addEventListener("click", function(e) {
+    	e.preventDefault();
+      boton.classList.add("ocultar");
+      addTask();
     });
 
-    function addForm() {
-      var formulario = document.createElement("form");
-      taskContainer.insertBefore(formulario, taskContainer.childNodes[0]);
+    function addTask() {
+        
+        var entrada = document.createElement("input");
+        board.insertBefore(entrada, board.childNodes[0]);
+        entrada.classList.add("agregarTexto");
+        entrada.setAttribute("type", "text");
+        entrada.setAttribute("placeholder", "Añadir una lista...")
 
-      var publicacion = document.createElement("input");
-      formulario.insertBefore(publicacion, formulario.childNodes[0]).classList.add("agregarTexto");
+        var boton = document.createElement("button");
+        var textoBoton = document.createTextNode("Guardar");
+        boton.appendChild(textoBoton);
+        board.insertBefore(boton, board.childNodes[1]);
+        boton.classList.add("boton");
+        boton.setAttribute("type", "submit");
 
-      var boton = document.createElement("button");
-      var texto = document.createTextNode("Guardar");
-      boton.appendChild(texto);
-      formulario.insertBefore(boton, formulario.childNodes[1]).classList.add("boton");
+        container.classList.add("caja");
 
-      taskContainer.classList.add("caja");
+        boton.addEventListener("click", function(e) {
+            e.preventDefault();
+
+            entrada.classList.add("ocultar");
+            boton.classList.add("ocultar");
+
+            var texto = entrada.value;
+            var titulo = document.createElement("div");
+            titulo.innerHTML = texto;
+            board.insertBefore(titulo, board.childNodes[0]);
+            titulo.classList.add("titulo");
+
+            var vinculo = document.createElement("a");
+            var textoVinculo = document.createTextNode("Añadir una tarjeta...");
+            vinculo.appendChild(textoVinculo);
+            board.insertBefore(vinculo, board.childNodes[1]);
+            vinculo.classList.add("vinculo");
+        });
     }
-});
+}); 
