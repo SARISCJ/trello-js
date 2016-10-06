6dbbacb86e600aa206ca7b7918892e1e8e8ac3ab
SARISCJ <saracastillo0@gmail.com>
2016-10-06 18:59:38 -0500

    trello primer trabajo terminado 6 versiones

--- js/app.js
+++ js/app.js
@@ -1,49 +1,92 @@
-window.addEventListener("load", function() {
+window.addEventListener("load", function(){
+  var contenedorPadre= document.getElementById("contenedorPadre");
+  var padre = document.getElementById("padre");
+  var lista = document.getElementById("lista");
+  var formulario = document.getElementById("formulario");
+  var button = document.getElementById("boton");
+  var div = document.getElementById("div");
+  var contador= 1;
+      
+  div.addEventListener("click", function() {
+    agregar(formulario, div);
+    lista.focus();
+  });
 
-    var container = document.getElementById("task-container");
-    var boton = document.getElementById("boton-task");
-    var board = document.getElementById("board");
+  button.addEventListener("click", function(e){
+    e.preventDefault();
+    var texto = "<a>Añadir una tarjeta...</a>";
+    var conedorHermano = document.createElement("div");
+    conedorHermano.classList.add("padre");
+
+    var padreTemporal = formulario.parentNode;
+
+    contenedorPadre.appendChild(conedorHermano);
+    conedorHermano.appendChild(formulario);
+    conedorHermano.appendChild(div);
+
+    padreTemporal.remove();
+
+    var contenedor = document.createElement("div");
+    contenedor.classList.add("formulario");
+    contenedor.classList.add("width");
+    contenedor.classList.add("inline-block");
+    contenedorPadre.insertBefore(contenedor, contenedorPadre.lastElementChild);
     
-    boton.addEventListener("click", function(e) {
-    	e.preventDefault();
-      boton.classList.add("ocultar");
-      addTask();
-    });
+    agregar(formulario, div);
 
-    function addTask() {
-        
-        var entrada = document.createElement("input");
-        board.insertBefore(entrada, board.childNodes[0]);
-        entrada.classList.add("agregarTexto");
-        entrada.setAttribute("type", "text");
-        entrada.setAttribute("placeholder", "Añadir una lista...")
+    crear("div", lista.value, contenedor, "negrita");
+    crear("div", texto, contenedor, "grey");
 
-        var boton = document.createElement("button");
-        var textoBoton = document.createTextNode("Guardar");
-        boton.appendChild(textoBoton);
-        board.insertBefore(boton, board.childNodes[1]);
-        boton.classList.add("boton");
-        boton.setAttribute("type", "submit");
+    var select = document.querySelectorAll(".grey");
 
-        container.classList.add("caja");
-
-        boton.addEventListener("click", function(e) {
-            e.preventDefault();
-
-            entrada.classList.add("ocultar");
-            boton.classList.add("ocultar");
-
-            var texto = entrada.value;
-            var titulo = document.createElement("div");
-            titulo.innerHTML = texto;
-            board.insertBefore(titulo, board.childNodes[0]);
-            titulo.classList.add("titulo");
-
-            var vinculo = document.createElement("a");
-            var textoVinculo = document.createTextNode("Añadir una tarjeta...");
-            vinculo.appendChild(textoVinculo);
-            board.insertBefore(vinculo, board.childNodes[1]);
-            vinculo.classList.add("vinculo");
-        });
+    for(var i=0; i<select.length; i++){
+      select[i].addEventListener("click", function(){
+        this.classList.add("none");
+        crearFormulario("form", contenedor, "formulario", this);
+      });
     }
-}); 
+
+    lista.value="";
+  });
+
+  var close = document.getElementById("close");
+  close.addEventListener("click", function(e) {
+    e.preventDefault();
+    agregar(formulario, div);
+  });
+
+  function crearFormulario(formulario, padre, clase1, select){
+    var formula = document.createElement(formulario);
+    formula.classList.add(clase1);
+    padre.appendChild(formula);
+
+    crear("textarea", "", formula, "textArea");
+    crear("button", "Guardar", formula, "button");
+
+    formula.lastElementChild.addEventListener("click", function(e){
+      e.preventDefault();
+      select.classList.remove("none");
+
+      var mensajes = document.createElement("div");
+      mensajes.ID= "mensaje" + contador;
+      mensajes.innerHTML=formula.firstElementChild.value;
+      formula.classList.add("none");
+      mensajes.classList.add("mensajes");
+      padre.insertBefore(mensajes, select);
+      contador++;
+    });
+  }
+
+  function crear(div, contenido,padre, clase1){
+    var titulo = document.createElement(div);
+    titulo.innerHTML = contenido;
+    titulo.classList.add("padding");
+    titulo.classList.add(clase1);
+    padre.appendChild(titulo);
+  }
+
+  function agregar(formulario, div){
+    formulario.classList.toggle("none");
+    div.classList.toggle("none");
+  }
+});
